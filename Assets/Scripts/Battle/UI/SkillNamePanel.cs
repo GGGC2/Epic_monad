267b31ad0a388d0e752c98baa9bad1289b0bb51e
ParkJuhyeong <majecty@gmail.com>
2016-08-26 12:04:25 +0900

    Fix skillnamepanel

--- Assets/Scripts/Battle/UI/SkillNamePanel.cs
+++ Assets/Scripts/Battle/UI/SkillNamePanel.cs
@@ -7,23 +7,32 @@ public class SkillNamePanel : MonoBehaviour
 {
     GameObject text;
     GameObject background;
+    GameObject background2;
 
     void Awake()
     {
         text = transform.Find("Text").gameObject;
         background = transform.Find("Background").gameObject;
+        background2 = transform.Find("Background2").gameObject;
+    }
+
+    void Start()
+    {
+        Hide();
     }
 
     public void Hide()
     {
         text.GetComponent<Text>().text = "";
         background.GetComponent<Image>().enabled = false;
+        background2.GetComponent<Image>().enabled = false;
     }
 
     public void Set(string skillName)
     {
         text.GetComponent<Text>().text = skillName;
         background.GetComponent<Image>().enabled = true;
+        background2.GetComponent<Image>().enabled = true;
     }
 }
 }
