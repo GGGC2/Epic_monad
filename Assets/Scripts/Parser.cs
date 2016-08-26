b76410824283d97e9437770ffd5243aa802dd416
ParkJuhyeong <majecty@gmail.com>
2016-08-26 12:21:22 +0900

    Fix map loading bug
    Other stage's map was loaded.

--- Assets/Scripts/Parser.cs
+++ Assets/Scripts/Parser.cs
@@ -11,16 +11,16 @@ public class Parser : MonoBehaviour {
 	public static List<DialogueData> GetParsedDialogueData(TextAsset dialogueDataFile)
 	{
 		List<DialogueData> dialogueDataList = new List<DialogueData>();
-		
+
 		string csvText = dialogueDataFile.text;
 		string[] unparsedDialogueDataStrings = csvText.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
-		
+
 		for (int i = 0; i < unparsedDialogueDataStrings.Length; i++)
 		{
 			DialogueData dialogueData = new DialogueData(unparsedDialogueDataStrings[i]);
 			dialogueDataList.Add(dialogueData);
 		}
-		
+
 		return dialogueDataList;
 	}
 
@@ -33,13 +33,13 @@ public class Parser : MonoBehaviour {
 		{
 			BattleEndTrigger battleEndTrigger = new BattleEndTrigger();
 			battleEndTriggers.Add(battleEndTrigger);
-			return battleEndTriggers; 
+			return battleEndTriggers;
 		}
 
-		csvFile = FindObjectOfType<StageManager>().battleEndConditionData as TextAsset;
+		csvFile = FindObjectOfType<StageManager>().GetBattleEndConditionData() as TextAsset;
 		string csvText = csvFile.text;
 		string[] unparsedBattleEndConditionDataStrings = csvText.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
-		
+
 		for (int i = 1; i < unparsedBattleEndConditionDataStrings.Length; i++)
 		{
 			BattleEndTrigger battleEndTrigger = new BattleEndTrigger(unparsedBattleEndConditionDataStrings[i]);
@@ -52,15 +52,15 @@ public class Parser : MonoBehaviour {
 	public static List<UnitInfo> GetParsedUnitInfo()
 	{
 		List<UnitInfo> unitInfoList = new List<UnitInfo>();
-		
+
 		TextAsset csvFile;
 		if (FindObjectOfType<StageManager>() != null)
-			csvFile = FindObjectOfType<StageManager>().unitData as TextAsset;
+			csvFile = FindObjectOfType<StageManager>().GetUnitData() as TextAsset;
 		else
 			csvFile = Resources.Load("Data/testStageUnitData") as TextAsset;
 		string csvText = csvFile.text;
 		string[] unparsedUnitInfoStrings = csvText.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
-		
+
 		for (int i = 1; i < unparsedUnitInfoStrings.Length; i++)
 		{
 			try
@@ -76,10 +76,10 @@ public class Parser : MonoBehaviour {
 				throw e;
 			}
 		}
-		
+
 		return unitInfoList;
 	}
-	
+
 	private static List<SkillInfo> skillInfosCache;
 	public static List<SkillInfo> GetParsedSkillInfo()
 	{
@@ -89,17 +89,17 @@ public class Parser : MonoBehaviour {
 		}
 
 		List<SkillInfo> skillInfoList = new List<SkillInfo>();
-		
+
 		TextAsset csvFile = Resources.Load("Data/testSkillData") as TextAsset;
 		string csvText = csvFile.text;
 		string[] unparsedSkillInfoStrings = csvText.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
-		
+
 		for (int i = 1; i < unparsedSkillInfoStrings.Length; i++)
 		{
 			SkillInfo skillInfo = new SkillInfo(unparsedSkillInfoStrings[i]);
 			skillInfoList.Add(skillInfo);
 		}
-		
+
 		skillInfosCache = skillInfoList;
 		return skillInfoList;
 	}
@@ -150,36 +150,36 @@ public class Parser : MonoBehaviour {
 			throw e;
 		}
 	}
-    
+
     public static List<StatusEffectInfo> GetParsedStatusEffectInfo()
     {
         List<StatusEffectInfo> statusEffectInfoList = new List<StatusEffectInfo>();
-        
+
         TextAsset csvFile = Resources.Load("Data/testStatusEffectData") as TextAsset;
 		string csvText = csvFile.text;
 		string[] unparsedStatusEffectInfoStrings = csvText.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
-		
+
 		for (int i = 1; i < unparsedStatusEffectInfoStrings.Length; i++)
 		{
 			StatusEffectInfo statusEffectInfo = new StatusEffectInfo(unparsedStatusEffectInfoStrings[i]);
 			statusEffectInfoList.Add(statusEffectInfo);
 		}
-		
+
 		return statusEffectInfoList;
     }
-	
+
 	public static List<TileInfo> GetParsedTileInfo()
 	{
 		List<TileInfo> tileInfoList = new List<TileInfo>();
-		
+
 		TextAsset csvFile;
 		if (FindObjectOfType<StageManager>() != null)
-			csvFile = FindObjectOfType<StageManager>().mapData as TextAsset;
+			csvFile = FindObjectOfType<StageManager>().GetMapData() as TextAsset;
 		else
 			csvFile = Resources.Load("Data/testMapData") as TextAsset;
 		string csvText = csvFile.text;
 		string[] unparsedTileInfoStrings = csvText.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
-		
+
 		for (int reverseY = unparsedTileInfoStrings.Length -1; reverseY >= 0 ; reverseY--)
 		{
 			string[] parsedTileInfoStrings = unparsedTileInfoStrings[reverseY].Split(',');
@@ -191,7 +191,7 @@ public class Parser : MonoBehaviour {
 				tileInfoList.Add(tileInfo);
 			}
 		}
-		
+
 		return tileInfoList;
 	}
 
