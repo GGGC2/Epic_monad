681f7ee9b37da1e90e18fea82ee15939f2e2daf9
Minjung Kim <Reny.atkins@gmail.com>
2017-02-21 05:42:12 +0900

    Implement Lenien's passive

---
+++ Assets/Scripts/Battle/Skills/Lenien_2_r_SkillLogic.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Linq;
+
+namespace Battle.Skills
+{
+public class Lenien_2_r_SkillLogic : BasePassiveSkillLogic {
+
+	public override float ApplyIgnoreDefenceByEachPassive(float defense, Unit caster, Unit target)
+	{
+		int partyLevel = MonoBehaviour.FindObjectOfType<BattleManager>().GetPartyLevel();
+		float ignoreAmountPerLevel = 0.6f;
+		float baseAmountPerLevel = 42;
+
+		TileManager tileManager = MonoBehaviour.FindObjectOfType<TileManager>();
+		Tile tileUnderTarget = tileManager.GetTile(target.GetPosition()).GetComponent<Tile>();
+		if (tileUnderTarget.GetTileElement() == Enums.Element.Water)
+			defense -= baseAmountPerLevel + (ignoreAmountPerLevel * partyLevel);
+
+		return defense;
+	}
+}
+}
