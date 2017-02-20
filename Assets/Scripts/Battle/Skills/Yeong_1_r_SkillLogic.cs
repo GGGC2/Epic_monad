9727af5c9d42d60a34a10bb620f5c10d0c12f7d1
Minjung Kim <Reny.atkins@gmail.com>
2017-02-20 22:10:43 +0900

    Fix indenting

--- Assets/Scripts/Battle/Skills/Yeong_1_r_SkillLogic.cs
+++ Assets/Scripts/Battle/Skills/Yeong_1_r_SkillLogic.cs
@@ -4,7 +4,7 @@ using Battle.Damage;
 namespace Battle.Skills
 {
 public class Yeong_1_r_SkillLogic : BasePassiveSkillLogic
-	{
+{
 	public override void triggerActiveSkillDamageApplied(Unit yeong)
 	{
 		StatusEffector.AttachStatusEffect(yeong, this.passiveSkill, yeong);
