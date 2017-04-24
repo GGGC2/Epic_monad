f606c750bc9e0864d6c4267c802043c17599a7f3
Minjung Kim <Reny.atkins@gmail.com>
2017-04-24 20:42:37 +0900

    Implement yeong skill 2-l

---
+++ Assets/Scripts/Battle/Skills/Yeong_2_l_SkillLogic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Enums;
+
+namespace Battle.Skills
+{
+public class Yeong_2_l_SkillLogic : BaseSkillLogic {
+	public override void ApplyAdditionalDamage(SkillInstanceData skillInstanceData) 
+    {
+		int totalEvasion = 0;
+
+		List<StatusEffect> statusEffects = skillInstanceData.getCaster().GetStatusEffectList();
+		foreach (var statusEffect in statusEffects)
+		{
+			int num = statusEffect.fixedElem.actuals.Count;
+			for (int i = 0; i < num; i++)
+			{
+				if (statusEffect.IsOfType(i, StatusEffectType.EvasionChange))
+				{
+					totalEvasion += (int)statusEffect.GetAmount(i);
+				}
+			}
+		}
+
+		float damageBonus = (float)(100 + totalEvasion) / 100;
+		skillInstanceData.getDamage().relativeDamageBonus *= damageBonus;
+
+		Debug.Log("Evasion : " + totalEvasion + '\n' + "damage bonus : " + damageBonus);
+	}
+}
+}
