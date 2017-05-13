7252fcb9d4519025f3d5ccf659e3dd65a4414820
Minjung Kim <Reny.atkins@gmail.com>
2017-05-13 14:34:06 +0900

    에렌 저항력 무시 버그 수정

--- Assets/Scripts/Battle/Skills/Eren_2_r_SkillLogic.cs
+++ Assets/Scripts/Battle/Skills/Eren_2_r_SkillLogic.cs
@@ -10,12 +10,14 @@ namespace Battle.Skills
 public class Eren_2_r_SkillLogic : BasePassiveSkillLogic {
 	public override void ApplyStatusEffectByKill(HitInfo hitInfo, Unit deadUnit)
 	{
+		Debug.Log("AP before : " + hitInfo.caster.GetCurrentActivityPoint());
 		// 민첩성의 0.1만큼 행동력을 회복
 		Unit eren = hitInfo.caster;
 		int dexterity = eren.GetActualStat(Stat.Dexturity);
 		int amount = (int)(dexterity * 0.1f);
-
-		eren.RecoverActionPoint(amount);
+		Debug.Log("Amount : " + amount);
+		MonoBehaviour.FindObjectOfType<BattleManager>().StartCoroutine(eren.RecoverActionPoint(amount));
+		Debug.Log("AP before : " + hitInfo.caster.GetCurrentActivityPoint());
 	}
 }
 }
