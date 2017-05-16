39d87172af53fd13a0a1ea85e8be9c72bbd5b8b9
Minjung Kim <Reny.atkins@gmail.com>
2017-05-16 14:18:43 +0900

    카메라 이동 옵션 추가

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class CameraMover : MonoBehaviour {
 
+	public bool mouseMoveActive;
+	public bool keyboardMoveActive;
+
 	float speed = 0.1f;
 	Vector3 fixedPosition = Vector3.zero;
 
@@ -26,28 +29,34 @@ public class CameraMover : MonoBehaviour {
 		}
 
 		// move by mouse.
-		Vector3 mousePosition = Input.mousePosition;
+		if (mouseMoveActive)
+		{
+			Vector3 mousePosition = Input.mousePosition;
 
-		if (mousePosition.x < Screen.width*0.01f)
-			MoveCameraToDirection(Vector3.left);
-		else if (mousePosition.x > Screen.width*0.99f)
-			MoveCameraToDirection(Vector3.right);
+			if (mousePosition.x < Screen.width*0.01f)
+				MoveCameraToDirection(Vector3.left);
+			else if (mousePosition.x > Screen.width*0.99f)
+				MoveCameraToDirection(Vector3.right);
 
-		if (mousePosition.y < Screen.height*0.01f)
-			MoveCameraToDirection(Vector3.down);
-		else if (mousePosition.y > Screen.height*0.99f)
-			MoveCameraToDirection(Vector3.up);
+			if (mousePosition.y < Screen.height*0.01f)
+				MoveCameraToDirection(Vector3.down);
+			else if (mousePosition.y > Screen.height*0.99f)
+				MoveCameraToDirection(Vector3.up);
+		}
 
 		// move by keyboard.
-		if (Input.GetKey(KeyCode.LeftArrow))
-			MoveCameraToDirection(Vector3.left);
-		else if (Input.GetKey(KeyCode.RightArrow))
-			MoveCameraToDirection(Vector3.right);
+		if (keyboardMoveActive)
+		{
+			if (Input.GetKey(KeyCode.LeftArrow))
+				MoveCameraToDirection(Vector3.left);
+			else if (Input.GetKey(KeyCode.RightArrow))
+				MoveCameraToDirection(Vector3.right);
 
-		if (Input.GetKey(KeyCode.DownArrow))
-			MoveCameraToDirection(Vector3.down);
-		else if (Input.GetKey(KeyCode.UpArrow))
-			MoveCameraToDirection(Vector3.up);
+			if (Input.GetKey(KeyCode.DownArrow))
+				MoveCameraToDirection(Vector3.down);
+			else if (Input.GetKey(KeyCode.UpArrow))
+				MoveCameraToDirection(Vector3.up);
+		}
 	}
 
 	void MoveCameraToDirection(Vector3 direction)
