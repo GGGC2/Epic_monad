fa77949d73cc9369fe8f832cc97b142737b69ec1
Kim Yoonsung <yskim227@snu.ac.kr>
2017-06-03 10:48:17 +0900

    큐리 스킬 수정
    1. 유진의 '순수한 물', '청명수의 은총' 스킬로직을 '청명수의 축복' 스킬로직 파일에 작성한 것처럼 큐리의 '가연성 부착물' 스킬 로직을 '수상한 덩어리' 스킬로직 파일에 작성함.

--- Assets/Scripts/Battle/Skills/Curi/Curi_1_m_SkillLogic.cs
+++ Assets/Scripts/Battle/Skills/Curi/Curi_1_m_SkillLogic.cs
@@ -1,6 +1,37 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using Enums;
+using UnityEngine;
+
 namespace Battle.Skills {
     public class Curi_1_m_SkillLogic : BaseSkillLogic {
-        
+        public override IEnumerator TriggerStatusEffectsAtActionEnd(Unit target, StatusEffect statusEffect) { //'가연성 부착물' 스킬로직
+            if (statusEffect.GetDisplayName() == "가연성 부착물") {
+                Unit caster = statusEffect.GetCaster();
+                Tile tileUnderUnit = target.GetTileUnderUnit();
+                if (tileUnderUnit.GetTileElement() == Element.Fire) {
+                    DamageCalculator.AttackDamage damage = new DamageCalculator.AttackDamage();
+                    damage.resultDamage = 0.3f * caster.GetStat(Stat.Power);
+
+                    TileManager tileManager = MonoBehaviour.FindObjectOfType<TileManager>();
+                    List<Tile> tileList = tileManager.GetTilesInRange(RangeForm.Diamond, target.GetPosition(), 0, 1, Direction.Left);
+
+                    List<Unit> damagedUnitList = new List<Unit>();
+                    damagedUnitList.Add(target);
+                    foreach (Tile tile in tileList) {
+                        if (tile.IsUnitOnTile()) {
+                            Unit secondaryTarget = tile.GetUnitOnTile();
+                            damagedUnitList.Add(secondaryTarget);
+                        }
+                    }
+                    SkillInstanceData skillInstanceData = new SkillInstanceData(damage, statusEffect.GetOriginSkill(),
+                        caster, damagedUnitList, target, damagedUnitList.Count);
+                    foreach (var secondaryTarget in damagedUnitList) {
+                        yield return secondaryTarget.Damaged(skillInstanceData, true);
+                    }
+                }
+                target.RemoveStatusEffect(statusEffect);
+            }
+        }
     }
 }
