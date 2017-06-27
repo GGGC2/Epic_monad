8a27f5a4f42962d12b571ff3eae634b7f04d6db9
Orchid <orchidbird2@gmail.com>
2017-06-28 07:05:39 +0900

    BattleReady 개발중

--- Assets/Scripts/ReadyManager.cs
+++ Assets/Scripts/ReadyManager.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+public class ReadyManager : MonoBehaviour{
+	TextAsset csvFile;
+	void Start()
+	{
+		csvFile = Resources.Load("Data/StageAvailablePC", typeof(TextAsset)) as TextAsset;
+		string dataString = csvFile.text;
+		string[] unparsedDataStrings = dataString.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+		string Data = "";
 
-public class ReadyManager : MonoBehaviour
-{
-	void Start () 
-	{
-		
-	}
-	
-	void Update () 
-	{
-		
+		foreach(string TempData in unparsedDataStrings)
+		{
+			string[] TempElements = TempData.Split('\t');
+			if(TempElements[0] == SceneData.nextStageName)
+			{
+				Data = TempData;
+				break;
+			}
+		}
+
+		string[] elements = Data.Split('\t');
+		for(int i = 1; i < elements.Length; i++)
+		{
+
+		}
 	}
 }
