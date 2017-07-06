47eab761f80db7edfbc66927ab855740fc1f2f18
Orchid <orchidbird2@gmail.com>
2017-07-06 12:54:51 +0900

    .

--- Assets/Scripts/AvailableUnitPanel.cs
+++ Assets/Scripts/AvailableUnitPanel.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class AvailableUnitPanel : MonoBehaviour, IPointerEnterHandler{
+public class AvailableUnitPanel : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler{
 	public string unitName;
 
 	void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
 	{
 		Debug.Log("PointerEnter");
 		GameObject.Find("CharacterIllust").GetComponent<Image>().sprite = Resources.Load<Sprite>("StandingImage/"+unitName+"_standing");
+	}
+
+	void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+	{
+		
 	}
 
 	public void ChangeIllust()
