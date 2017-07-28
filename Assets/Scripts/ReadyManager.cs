656c303f5e035d8500f3f69203c73c01c2482df1
Orchid <orchidbird2@gmail.com>
2017-07-29 03:19:58 +0900

    Ready씬에서 AvailableUnit19,20이 비활성화되지 않던 버그 해결

--- Assets/Scripts/ReadyManager.cs
+++ Assets/Scripts/ReadyManager.cs
@@ -14,10 +14,10 @@ public class ReadyManager : MonoBehaviour{
 		csvFile = Resources.Load<TextAsset>("Data/StageAvailablePC");
 		string[] stageData = Parser.FindRowDataOf(csvFile.text, SceneData.stageNumber.ToString());
 
-		for (int i = 2; i < 20; i++)
+		for (int i = 2; i < 22; i++)
 		{
 			GameObject availableUnitPanel = GameObject.Find("AvailableUnit" + (i-1));
-			if (i < stageData.Length)				
+			if (i < stageData.Length)
 				availableUnitPanel.GetComponent<UnitPanel>().SetNameAndSprite(stageData[i]);
 			else
 				availableUnitPanel.gameObject.SetActive(false);
