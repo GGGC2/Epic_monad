a9f516380b3e35804c75fa30c706f9c62b5d0262
Kim Minryoung <minryoung93@gmail.com>
2017-07-30 15:13:13 +0900

    그레네브 단검술 가끔 발동 안 되는 버그 수정

--- Assets/Scripts/Battle/Skills/Grenev/Grenev_1_m_SkillLogic.cs
+++ Assets/Scripts/Battle/Skills/Grenev/Grenev_1_m_SkillLogic.cs
@@ -6,23 +6,24 @@ using System.Collections;
 namespace Battle.Skills {
     class Grenev_1_m_SkillLogic : BaseSkillLogic {
         public override bool CheckApplyPossible(Unit caster, List<Tile> targetTiles) {
-            TileManager tileManager = MonoBehaviour.FindObjectOfType<TileManager>();
-            Tile tile = targetTiles[0];
-            if(tile.IsUnitOnTile()) {
-                Unit target = tile.GetUnitOnTile();
-                Vector2 targetDirection = tileManager.ToVector2(target.GetDirection());
-                Vector2 skillDirection = (target.GetPosition() - caster.GetPosition()).normalized;
-                if(targetDirection == skillDirection) {
-                    return true;
-                }
-            }
+			TileManager tileManager = MonoBehaviour.FindObjectOfType<TileManager>();
+			foreach(Tile tile in targetTiles){
+				if(tile.IsUnitOnTile()) {
+					Unit target = tile.GetUnitOnTile();
+					Vector2 targetDirection = tileManager.ToVector2(target.GetDirection());
+					Vector2 skillDirection = (target.GetPosition() - caster.GetPosition()).normalized;
+					if(targetDirection == skillDirection) {
+						return true;
+					}
+				}
+			}
             return false;
         }
         public override bool IgnoreShield(SkillInstanceData skillInstanceData) {
             return true;
         }
         public override float ApplyIgnoreDefenceAbsoluteValueBySkill(float defense, Unit caster, Unit target) {
-            return 0;
+			return System.Math.Min(0, defense);
         }
     }
 }
