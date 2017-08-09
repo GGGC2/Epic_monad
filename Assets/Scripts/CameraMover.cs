e53f81495e63c76fcb149f25830f574c72df277a
Kim Yoonsung <yskim227@snu.ac.kr>
2017-08-09 21:44:32 +0900

    카메라가 정해진 범위(타일들에 의해 결정됨) 내에서만 움직이게 수정

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using Enums;
+using System.Linq;
 
 public class CameraMover : MonoBehaviour {
 
 	public bool mouseMoveActive;
 	public bool keyboardMoveActive;
+    Dictionary<Direction, float> boundary = new Dictionary<Direction, float>();
+    const float MARGIN = 1;
+
 
 	float speed = 4f;
 	Vector3 fixedPosition = Vector3.zero;
@@ -13,7 +18,33 @@ public class CameraMover : MonoBehaviour {
 	{
 		fixedPosition = new Vector3(position.x, position.y, transform.position.z);
 	}
+
+    bool isAtBoundary(Direction direction) {
+        switch(direction) {
+        case Direction.Left:
+            return Camera.main.transform.position.x <= boundary[direction];
+        case Direction.Right:
+            return Camera.main.transform.position.x >= boundary[direction];
+        case Direction.Up:
+            return Camera.main.transform.position.y >= boundary[direction];
+        case Direction.Down:
+            return Camera.main.transform.position.y <= boundary[direction];
+        }
+        return true;
+    }
 	
+    void Start() {
+    }
+    public void CalculateBoundary() {
+
+        List<Tile> tiles = new List<Tile>();
+        Dictionary<Vector2, Tile> tilesDict = FindObjectOfType<TileManager>().GetAllTiles();
+        foreach (var kv in tilesDict) tiles.Add(kv.Value);
+        boundary[Direction.Left] = tiles.Min(tile => tile.transform.position.x) - MARGIN;
+        boundary[Direction.Right] = tiles.Max(tile => tile.transform.position.x) + MARGIN;
+        boundary[Direction.Up] = tiles.Max(tile => tile.transform.position.y) + MARGIN;
+        boundary[Direction.Down] = tiles.Min(tile => tile.transform.position.y) - MARGIN;
+    }
 	void Update () {
 		// return to fixedPosition.
 		if (Input.GetKeyDown(KeyCode.Space))
@@ -27,28 +58,28 @@ public class CameraMover : MonoBehaviour {
 		{
 			Vector3 mousePosition = Input.mousePosition;
 
-			if (mousePosition.x < Screen.width*0.01f)
+			if (mousePosition.x < Screen.width*0.01f && !isAtBoundary(Direction.Left))
 				MoveCameraToDirection(Vector3.left);
-			else if (mousePosition.x > Screen.width*0.99f)
+			else if (mousePosition.x > Screen.width* 0.99f && !isAtBoundary(Direction.Right))
 				MoveCameraToDirection(Vector3.right);
 
-			if (mousePosition.y < Screen.height*0.01f)
+			if (mousePosition.y < Screen.height* 0.01f && !isAtBoundary(Direction.Down))
 				MoveCameraToDirection(Vector3.down);
-			else if (mousePosition.y > Screen.height*0.99f)
+			else if (mousePosition.y > Screen.height* 0.99f && !isAtBoundary(Direction.Up))
 				MoveCameraToDirection(Vector3.up);
 		}
 
 		// move by keyboard.
 		if (keyboardMoveActive)
 		{
-			if (Input.GetKey(KeyCode.LeftArrow))
+			if (Input.GetKey(KeyCode.LeftArrow) && !isAtBoundary(Direction.Left))
 				MoveCameraToDirection(Vector3.left);
-			else if (Input.GetKey(KeyCode.RightArrow))
+			else if (Input.GetKey(KeyCode.RightArrow) && !isAtBoundary(Direction.Right))
 				MoveCameraToDirection(Vector3.right);
 
-			if (Input.GetKey(KeyCode.DownArrow))
+			if (Input.GetKey(KeyCode.DownArrow) && !isAtBoundary(Direction.Down))
 				MoveCameraToDirection(Vector3.down);
-			else if (Input.GetKey(KeyCode.UpArrow))
+			else if (Input.GetKey(KeyCode.UpArrow) && !isAtBoundary(Direction.Up))
 				MoveCameraToDirection(Vector3.up);
 		}
 	}
