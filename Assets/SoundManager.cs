59c006e1a40cee45ed96169b7f7e703efaf4a556
Kim Minryoung <minryoung93@gmail.com>
2017-08-11 19:19:13 +0900

    SoundManager Awake시 모든 sound 로드하게 수정

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -1,48 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour {
 	private static SoundManager instance = null;
 	public static SoundManager Instance { get { return instance; } }
 
-	BattleManager battleManager;
 	AudioSource audioSource;
 
-	public AudioClip musicPrelude;
-	public AudioClip musicNormal;
-	public AudioClip musicTense;
+	static Dictionary<string, AudioClip> BGMs;
+	static Dictionary<string, AudioClip> SEs;
 
 	public void PlayBgm(string name){
-		if(name == "Script_Prelude")
-			audioSource.clip = musicPrelude;
-		else if(name == "Script_Normal")
-			audioSource.clip = musicNormal;
-		else if(name == "Script_Tense")
-			audioSource.clip = musicTense;
-		else{
-			AudioClip bgm = Resources.Load<AudioClip>("Sound/" + name);
-			if(audioSource.clip == null || audioSource.clip != bgm)
-				audioSource.clip = bgm;
-		}
+		AudioClip bgm = BGMs [name];
+		if(audioSource.clip == null || audioSource.clip != bgm)
+			audioSource.clip = bgm;
 		if(!audioSource.isPlaying)
 			audioSource.Play();
 	}
 	public void PlaySE(string name){
-		AudioClip SE = Resources.Load<AudioClip>("Sound/SE/" + name);
-		audioSource.PlayOneShot (SE);
+		audioSource.PlayOneShot (SEs[name]);
 	}
 
 	void Awake () {
-		if (instance != null && instance != this){
-			Destroy(this.gameObject);
+		if (instance != null && instance != this) {
+			Destroy (this.gameObject);
 			return;
-		}else
+		}
+		else {
 			instance = this;
+		}
 
 		DontDestroyOnLoad(this.gameObject);
-
-		battleManager = FindObjectOfType<BattleManager>();
+		LoadBGMsAndSEs ();
 		audioSource = gameObject.GetComponent<AudioSource>();
+	}
+
+	static void LoadBGMsAndSEs(){
+		AudioClip[] BGMLists = Resources.LoadAll<AudioClip> ("BGMs");
+		BGMs = new Dictionary<string, AudioClip> ();
+		foreach (AudioClip BGM in BGMLists) {
+			BGMs [BGM.name] = BGM;
+		}
+		AudioClip[] SELists = Resources.LoadAll<AudioClip> ("SEs");
+		SEs = new Dictionary<string, AudioClip> ();
+		foreach (AudioClip SE in SELists) {
+			SEs [SE.name] = SE;
+		}
 	}
 }
