7ce1a6782a9356e797ba352612cc0efe9c7192f0
Kim Minryoung <minryoung93@gmail.com>
2017-08-15 22:26:41 +0900

    오브젝트 기술 때문에 죽은 유닛이 다음 페이즈에 처음으로 행동하면 죽는 버그 fix

--- Assets/Scripts/Battle/Turn/ObjectUnitBehaviour.cs
+++ Assets/Scripts/Battle/Turn/ObjectUnitBehaviour.cs
@@ -14,10 +14,11 @@ namespace Battle.Turn
 				unit.SetNotAlreadyBehavedObject ();
 			}
 			while (true) {
-				//yield return BattleData.battleManager.BeforeActCommonAct ();
+				yield return BattleData.battleManager.BeforeActCommonAct ();
 				//오브젝트 때문에 오브젝트가 죽을 수도 있으니 하나 행동 끝날 때마다 매번 오브젝트유닛 목록을 다시 받아온다
 				objectUnits = BattleData.GetObjectUnitsList();
 				Unit selectedObjectUnit = GetNotAlreadyBehavedObjectUnit (objectUnits);
+				BattleData.selectedUnit = selectedObjectUnit;
 				if (selectedObjectUnit == null)
 					break;
 				yield return AnObjectUnitBehave (selectedObjectUnit);
