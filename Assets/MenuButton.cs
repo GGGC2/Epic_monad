faeda5e85843000d3bc65a916bd61bf643a7a5b7
Minjung Kim <Reny.atkins@gmail.com>
2017-08-25 03:43:43 +0900

    배틀씬 우상단 메뉴버튼 고침

--- Assets/MenuButton.cs
+++ Assets/MenuButton.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuButton : MonoBehaviour{
 	//유니티 버튼 함수는 parameter를 하나밖에 줄 수 없어서 함수를 쪼개놓음
@@ -10,5 +11,9 @@ public class MenuButton : MonoBehaviour{
 
 	public void DeactivateObject(GameObject TargetObject){
 		TargetObject.SetActive(false);
+	}
+
+	public void GoToTitleScene(){
+		SceneManager.LoadScene("Title");
 	}
 }
