d1fd537385fe1fbca58a1c32673dd53aef07fda4
Minjung Kim <Reny.atkins@gmail.com>
2017-09-02 09:45:35 +0900

    파티선택창 왼쪽 부분 구현

---
+++ Assets/AvailableUnitButton.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Enums;
+
+public class AvailableUnitButton : MonoBehaviour {
+
+	Image standingImage;
+	Text unitName;
+	Image classImage;
+	Image celestialImage;
+	Image elementImage;
+
+	// Use this for initialization
+	void Start () {
+		standingImage = transform.Find("CharacterImageMask").Find("CharacterImage").GetComponent<Image>();
+		unitName = transform.Find("NameText").GetComponent<Text>();
+		classImage = transform.Find("ClassImageMask").Find("ClassImage").GetComponent<Image>();
+		celestialImage = transform.Find("CelestialImageMask").Find("CelestialImage").GetComponent<Image>();
+		elementImage = transform.Find("ElementImageMask").Find("ElementImage").GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetNameAndSprite(string nameString) {
+		unitName.text = UnitInfo.ConvertToKoreanName(nameString);
+
+		if(nameString == "unselected")
+			standingImage.sprite = Resources.Load<Sprite>("transparent");
+		else
+			standingImage.sprite = Resources.Load<Sprite>("StandingImage/"+nameString+"_standing");		
+	
+		SetClassImage(UnitInfo.GetUnitClass(nameString));
+		SetElementImage(UnitInfo.GetElement(nameString));
+		SetCelestialImage(UnitInfo.GetCelestial(nameString));
+	}
+
+	public void SetClassImage(UnitClass unitClass) {
+        if (unitClass == UnitClass.Melee)
+            classImage.sprite = Resources.Load<Sprite>("Icon/Stat/meleeClass");
+        else if (unitClass == UnitClass.Magic)
+            classImage.sprite = Resources.Load<Sprite>("Icon/Stat/magicClass");
+        else
+            classImage.sprite = Resources.Load<Sprite>("Icon/Empty");
+    }
+
+    public void SetElementImage(Element element) {
+        if (element == Element.Fire)
+            elementImage.sprite = Resources.Load("Icon/Element/fire", typeof(Sprite)) as Sprite;
+        else if (element == Element.Water)
+            elementImage.sprite = Resources.Load("Icon/Element/water", typeof(Sprite)) as Sprite;
+        else if (element == Element.Plant)
+            elementImage.sprite = Resources.Load("Icon/Element/plant", typeof(Sprite)) as Sprite;
+        else if (element == Element.Metal)
+            elementImage.sprite = Resources.Load("Icon/Element/metal", typeof(Sprite)) as Sprite;
+        else
+            elementImage.sprite = Resources.Load("Icon/Empty", typeof(Sprite)) as Sprite;
+    }
+
+    public void SetCelestialImage(Celestial celestial) {
+        if (celestial == Celestial.Sun)
+            celestialImage.sprite = Resources.Load("Icon/Celestial/sun", typeof(Sprite)) as Sprite;
+        else if (celestial == Celestial.Moon)
+            celestialImage.sprite = Resources.Load("Icon/Celestial/moon", typeof(Sprite)) as Sprite;
+        else if (celestial == Celestial.Earth)
+            celestialImage.sprite = Resources.Load("Icon/Celestial/earth", typeof(Sprite)) as Sprite;
+        else
+            celestialImage.sprite = Resources.Load("Icon/Empty", typeof(Sprite)) as Sprite;
+    }
+}
--- Assets/Scripts/ReadyManager.cs
+++ Assets/Scripts/ReadyManager.cs
@@ -10,38 +10,43 @@ public class ReadyManager : MonoBehaviour{
 	public List<UnitPanel> selected = new List<UnitPanel>();
 	public string currentUnitName;
 
+	List<GameObject> availableUnitButtons = new List<GameObject>();
+
 	public Button readyButton;
 
 	void Start(){
 		csvFile = Resources.Load<TextAsset>("Data/StageAvailablePC");
-		string[] stageData = Parser.FindRowDataOf(csvFile.text, SceneData.stageNumber.ToString());
+		// string[] stageData = Parser.FindRowDataOf(csvFile.text, SceneData.stageNumber.ToString());
+		string[] stageData = Parser.FindRowDataOf(csvFile.text, "2");
 
-		for (int i = 1; i < 21; i++){
-			GameObject availableUnitPanel = GameObject.Find("AvailableUnit" + i);
+
+		for (int i = 1; i <= 20; i++){
+			GameObject availableUnitButton = GameObject.Find("CharacterButton" + i);
+			availableUnitButtons.Add(availableUnitButton);
 			if (i <= Int32.Parse(stageData[2]))
-				availableUnitPanel.GetComponent<UnitPanel>().SetNameAndSprite(stageData[i+2]);
+				availableUnitButton.GetComponent<AvailableUnitButton>().SetNameAndSprite(stageData[i+2]);
 			else
-				availableUnitPanel.gameObject.SetActive(false);
+				availableUnitButton.SetActive(false);
 		}
 
-		for (int i = 1; i <= 8; i++){
-			UnitPanel Panel = GameObject.Find("SelectedUnit"+i).GetComponent<UnitPanel>();
-			if (i <= Int32.Parse(stageData[1])){
-				selected.Add(Panel);
-				Panel.unitName = "unselected";
-			}
-			else
-				Panel.gameObject.SetActive(false);
-		}
+		// for (int i = 1; i <= 8; i++){
+		// 	UnitPanel Panel = GameObject.Find("SelectedUnit"+i).GetComponent<UnitPanel>();
+		// 	if (i <= Int32.Parse(stageData[1])){
+		// 		selected.Add(Panel);
+		// 		Panel.unitName = "unselected";
+		// 	}
+		// 	else
+		// 		Panel.gameObject.SetActive(false);
+		// }
 
 		DontDestroyOnLoad(gameObject);
 	}
 
 	void Update(){
-		if (IsThereAnyReadiedUnit())
-			readyButton.interactable = true;
-		else
-			readyButton.interactable = false;
+		// if (IsThereAnyReadiedUnit())
+		// 	readyButton.interactable = true;
+		// else
+		// 	readyButton.interactable = false;
 
 		if(Input.GetKeyDown(KeyCode.A))
 			GameObject.Find("SceneLoader").GetComponent<SceneLoader>().LoadNextBattleScene();
