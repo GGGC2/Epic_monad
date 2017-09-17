367b67f357e8804adb9f70f84aa3ec7a799785b5
Orchid <orchidbird2@gmail.com>
2017-09-18 00:10:13 +0900

    밸런싱한 스킬 데이터 업데이트(2)

--- Assets/Scripts/Battle/Skills/Lenien/Lenien_2_r_SkillLogic.cs
+++ Assets/Scripts/Battle/Skills/Lenien/Lenien_2_r_SkillLogic.cs
@@ -7,8 +7,8 @@ public class Lenien_2_r_SkillLogic : BasePassiveSkillLogic {
 
 	public override float ApplyIgnoreResistanceAbsoluteValueByEachPassive(ActiveSkill appliedSkill, Unit target, Unit caster, float resistance)
 	{
-		float ignoreAmountPerLevel = 0.6f;
-		float baseAmountPerLevel = 42;
+		float ignoreAmountPerLevel = 0.7f;
+		float baseAmountPerLevel = 51;
 
 		TileManager tileManager = MonoBehaviour.FindObjectOfType<TileManager>();
 		Tile tileUnderTarget = tileManager.GetTile(target.GetPosition());
