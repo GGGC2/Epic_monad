04517328bed2b41aa63dc569403aaf7c374910bb
Orchid <orchidbird2@gmail.com>
2017-09-20 00:40:27 +0900

    각주의 항목이 10개를 초과할 때 페이지 넘기는 기능 추가

--- Assets/Scripts/UI/Glossary.cs
+++ Assets/Scripts/UI/Glossary.cs
@@ -6,20 +6,34 @@ using UnityEngine.UI;
 using GameData;
 
 public class Glossary : MonoBehaviour{
+	int itemPerPage = 10;
 	public GlossaryData.GlossaryType currentType;
-	public int page;
+	public int page; // 1이 아닌 0부터 시작
 	public Text InformationText;
+	public GameObject LeftArrow;
+	public GameObject RightArrow;
 	
 	public List<Text> ButtonTextList;
 
 	void OnEnable() {
+		InitializeAll();
+	}
+
+	void InitializeAll(){
+		page = 0;
+		InitializeExceptPage();
+	}
+
+	void InitializeExceptPage(){
+		SetArrowOnOff();
 		UpdateButtonName();
+		SetText(0);
 	}
 
 	public void SetText(int number){
 		if (number == 0) {
 			InformationText.text = "";	
-		} else {
+		}else{
 			GlossaryData searchResult = GlobalData.GlossaryDataList.Find (data => data.Type == currentType && data.index == number);
 			if (searchResult != null && searchResult.level > 0) {
 				InformationText.text = searchResult.text [searchResult.level];
@@ -29,19 +43,43 @@ public class Glossary : MonoBehaviour{
 
 	public void SetCurrentType(int number){
 		currentType = (GlossaryData.GlossaryType)number;
+		InitializeAll();
 	}
 
 	public void UpdateButtonName(){
 		for(int i = 0; i < 9; i++){
-			GlossaryData searchResult = GlobalData.GlossaryDataList.Find(data => data.Type == currentType && data.index == i+1);
+			GlossaryData searchResult = GlobalData.GlossaryDataList.Find(data => data.Type == currentType && data.index == page*itemPerPage+i+1);
 
 			if (searchResult != null && searchResult.level > 0) {
-				//Debug.Log("FOUND");
 				ButtonTextList [i].text = searchResult.name;
 			} else {
 				ButtonTextList [i].text = "";
 			}
 		}
+	}
+
+	public void MovePage(bool next){
+		if(next){
+			page += 1;
+		}else{
+			page -= 1;
+		}
+		InitializeExceptPage();
+	}
+
+	void SetArrowOnOff(){
+		LeftArrow.SetActive(true);
+		RightArrow.SetActive(true);
+		if(page == 0){
+			LeftArrow.SetActive(false);
+		}
+		if(page == CalcMaxPage()){
+			RightArrow.SetActive(false);
+		}
+	}
+
+	int CalcMaxPage(){
+		return (GlobalData.GlossaryDataList.FindAll(data => data.Type == currentType).Count-1)/itemPerPage;
 	}
 }
 
