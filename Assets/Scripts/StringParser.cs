ecc375b4429b3f3375fb43c557a373a93b832cbe
Orchid <orchidbird2@gmail.com>
2017-09-23 22:25:01 +0900

    Parsing 에러 발생했을 때 쉽게 찾도록 로그 추가

--- Assets/Scripts/StringParser.cs
+++ Assets/Scripts/StringParser.cs
@@ -16,7 +16,13 @@ public class StringParser{
 
 	public string Consume(){
 		index += 1;
-		return origin[index - 1];
+		try{
+			return origin[index - 1];
+		}catch{
+			Debug.LogError((index-1) + "번째 항목을 읽을 수 없습니다!");
+			Debug.LogError("행의 맨 앞 항목 : " + origin[0] + " / 마지막 항목 : " + origin[index-2]);
+			return "";
+		}
 	}
 
 	public bool ConsumeBool(){
@@ -33,17 +39,13 @@ public class StringParser{
 	}
 
 	 // true / false / none 처리용. 디폴트로 인식할 스트링과 그 결과값을 받는다.
-	public bool ConsumeBool(string defaultValue, bool result)
-	{
+	public bool ConsumeBool(string defaultValue, bool result){
 		string strValue = Consume();
-		try
-		{
+		try{
 			if (strValue == defaultValue) return result;
 			return bool.Parse(strValue);
-		}
-		catch (FormatException e)
-		{
-			Debug.LogError("Cannot parse boolean value " + strValue);
+		}catch{
+			Debug.LogError("Cannot parse boolean value of : " + strValue);
 			return false;
 		}
 	}
@@ -53,37 +55,30 @@ public class StringParser{
 		try{
 			return Int32.Parse(strValue);
 		}catch{
-			Debug.LogError("Cannot parse integer value " + strValue);
+			Debug.LogError("Cannot parse integer value of : " + strValue);
 			return -1;
 		}
 	}
 
-	public float ConsumeFloat()
-	{
+	public float ConsumeFloat(){
 		string strValue = Consume();
-		try
-		{
+		try{
 			return Single.Parse(strValue);
-		}
-		catch (FormatException e)
-		{
-			Debug.LogError("Cannot parse float value " + strValue);
+		}catch{
+			Debug.LogError("Cannot parse float value of : " + strValue);
 			return -1;
 		}
 	}
 
 	// 기본값이 있는 float 변수 파싱용. ConsumeString과 동일.
-	public float ConsumeFloat(string defaultValue, float f)
-	{
+	public float ConsumeFloat(string defaultValue, float f){
 		string strValue = Consume();
 		try
 		{	
 			if (strValue == defaultValue) return f;
 			return Single.Parse(strValue);
-		}
-		catch (FormatException e)
-		{
-			Debug.LogError("Cannot parse float value " + strValue);
+		}catch{
+			Debug.LogError("Cannot parse float value of : " + strValue);
 			return -1;
 		}
 	}
