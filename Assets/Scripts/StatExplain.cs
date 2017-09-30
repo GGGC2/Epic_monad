2e445e03839d2e7254a7579f2794eb673a7a8b9e
Orchid <orchidbird2@gmail.com>
2017-10-01 03:51:39 +0900

    2스테이지 배치 변경

--- Assets/Scripts/StatExplain.cs
+++ Assets/Scripts/StatExplain.cs
@@ -28,9 +28,9 @@ public class StatExplain : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
 			textUI.text = "마법 피해 " + Math.Round(statPoint*100/(statPoint+200), 1) + "% 감소";
 		}else if(stat == Stat.Agility){ //원래 Speed인데 목록에 없어서 대체
 			if(statPoint > 100){
-				textUI.text = "소모 행동력 " + Math.Round((statPoint-100)/(statPoint), 1) + "% 감소";
+				textUI.text = "소모 행동력 " + Math.Round((statPoint-100)*100/(statPoint), 1) + "% 감소";
 			}else if(statPoint < 100){
-				textUI.text = "소모 행동력 " + Math.Round((100-statPoint)/(statPoint), 1) + "% 증가";
+				textUI.text = "소모 행동력 " + Math.Round((100-statPoint)*100/(statPoint), 1) + "% 증가";
 			}else{
 				textUI.text = "소모 행동력 불변";
 			}
