31e31bccfad8b34a3c7836a5a4cde6b9bbbc9856
Kim Yoonsung <yskim227@snu.ac.kr>
2017-10-06 20:59:32 +0900

    버그 수정

--- Assets/Scripts/Battle/Skills/Eren/Eren_1_l_SkillLogic.cs
+++ Assets/Scripts/Battle/Skills/Eren/Eren_1_l_SkillLogic.cs
@@ -1,21 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using Enums;
 using System.Linq;
 
-namespace Battle.Skills
-{
-public class Eren_1_l_SkillLogic : BaseSkillLogic {
-	public override void ApplyAdditionalDamage(CastingApply castingApply) 
-    {
-		UnitStatusEffect uniqueStatusEffect = castingApply.GetCaster().StatusEffectList.Find(se => se.GetDisplayName() == "흡수");
-		
-		if (uniqueStatusEffect != null)
-		{
-			int stack = uniqueStatusEffect.GetRemainStack();
-			castingApply.GetDamage().baseDamage *= (1.0f + (0.2f * stack));
-		}
-	}
-}
+namespace Battle.Skills {
+    public class Eren_1_l_SkillLogic : BaseSkillLogic {
+        public override void ApplyAdditionalDamage(CastingApply castingApply) {
+            UnitStatusEffect uniqueStatusEffect = castingApply.GetCaster().StatusEffectList.Find(se => se.GetDisplayName() == "í씉ì닔");
+
+            if (uniqueStatusEffect != null) {
+                int stack = uniqueStatusEffect.GetRemainStack();
+                castingApply.GetDamage().baseDamage *= (1.0f + (0.2f * stack));
+            }
+        }
+    }
 }
