d22dbb9141f85bc2a45889a8ef55a1863d433e1f
Kim Yoonsung <yskim227@snu.ac.kr>
2017-10-10 10:34:07 +0900

    루키어스 스킬로직 수정

--- Assets/Scripts/Battle/Skills/Lucius/Lucius_2_m_SkillLogic.cs
+++ Assets/Scripts/Battle/Skills/Lucius/Lucius_2_m_SkillLogic.cs
@@ -10,12 +10,13 @@ namespace Battle.Skills {
             TileManager tileManager = TileManager.Instance;
             Vector2 casterPosition = caster.GetPosition();
             int count = 0;
-            List<Tile> tilesInRange = tileManager.GetTilesInRange(RangeForm.Diamond, casterPosition, 1, 1, 0, Direction.Down);
-            foreach (Tile tile in tilesInRange) {
-                if (!tileManager.isTilePassable(tile))
-                    count++;
+            foreach(Direction direction in Enum.GetValues(typeof(Direction))) {
+                if ((int)direction < 4) {
+                    Tile tile = tileManager.GetTile(casterPosition + Utility.ToVector2(direction));
+                    if (tile == null || !tileManager.isTilePassable(tile))
+                        count++;
+                }
             }
-
             UnitStatusEffect alreadyAppliedStatusEffect = caster.StatusEffectList.Find(se => se.GetOriginSkill() == passiveSkill);
             if (alreadyAppliedStatusEffect == null) {
             List<UnitStatusEffect> statusEffects = StatusEffector.AttachStatusEffect(caster, passiveSkill, caster);
