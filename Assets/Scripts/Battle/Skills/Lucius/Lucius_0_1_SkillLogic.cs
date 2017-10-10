fb87ebfee5d0f10909b06f8da03d6fd926c1f7d8
Kim Yoonsung <yskim227@snu.ac.kr>
2017-10-10 21:27:02 +0900

    루키어스 스킬로직 파일이 누락됨

---
+++ Assets/Scripts/Battle/Skills/Lucius/Lucius_0_1_SkillLogic.cs
@@ -0,0 +1,17 @@
+﻿
+using Enums;
+
+namespace Battle.Skills {
+    class Lucius_0_1_SkillLogic : BasePassiveSkillLogic {
+        public override void TriggerOnUnitDestroy(Unit unit, Unit destroyedUnit, TrigActionType actionType) {
+            if (unit.IsEnemy(destroyedUnit)) {
+                foreach (var otherUnit in UnitManager.Instance.GetAllUnits())
+                    if (unit.IsAlly(otherUnit))
+                        if (actionType == TrigActionType.Kill)
+                            otherUnit.ApplyDamageByNonCasting(10, unit, 0, 0, false, true, false);
+                        else if (actionType == TrigActionType.Retreat)
+                            otherUnit.RecoverActionPoint(10);
+            }
+        }
+    }
+}
